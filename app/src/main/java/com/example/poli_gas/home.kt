87f1d7cf2d212caa00ce34d5f35f2362d7bc5cc8
Bryan Zambrano <Bryanz0@users.noreply.github.com>
2020-01-02 15:02:09 -0500

    UI Design (Begin) >Welcome Land >Info User Land >Home Land
    Only UI Design, there are Lands static, there's no programing anything. The id's of all components are missing and menu needs to be convert to fragment

---
+++ app/src/main/java/com/example/poli_gas/home.kt
@@ -0,0 +1,12 @@
+package com.example.poli_gas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class home : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
