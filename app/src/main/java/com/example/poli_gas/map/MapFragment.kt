aadc9d27d33d9aa623a2c1314d1dd44936e175dd
alexarevalo9 <arevaloalex9@gmail.com>
2020-01-14 11:53:22 -0500

    add new features

---
+++ app/src/main/java/com/example/poli_gas/map/MapFragment.kt
@@ -0,0 +1,12 @@
+package com.example.poli_gas.map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MapFragment : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_map)
+    }
+}
